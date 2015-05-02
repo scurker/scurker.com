aea3a218934b7fb73fd7fca7b5675ff330ec592b
Jason <jason@scurker.com>
2015-05-02 10:17:39 -0500

    showing article year

---
+++ templates/helpers/article-year.js
@@ -0,0 +1,9 @@
+var currentYear;
+
+module.exports = function(date) {
+  var year = date.getFullYear();
+  if(year !== currentYear) {
+    currentYear = year;
+    return '<h3 class="year"><span>' + year + '</span></h3>';
+  }
+};
