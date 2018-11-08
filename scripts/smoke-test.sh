f676b85e82e0bc3f62cb880ac5f8093285cf83de
Jason <jason@scurker.com>
2018-11-08 11:58:24 -0600

    fix check for http status code

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -2,7 +2,7 @@
 
 HTTP_CODE=`curl -siL -o /dev/null -w '%{http_code}' https://scurker.com`
 
-if [[ $HTTP_CODE != $200 ]]; then
+if [[ $HTTP_CODE != "200" ]]; then
   echo "FAIL: status code $HTTP_CODE"
   exit 1
 fi
