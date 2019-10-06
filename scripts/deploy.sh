e26f84e64c80be07ac79efec6815ec8e9df3927f
Jason <jason@scurker.com>
2019-10-06 08:11:25 -0500

    remove old deploy steps

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git status
-git remote add deploy $remote
-git push deploy HEAD:master
--- scripts/smoke-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HTTP_CODE=`curl -siL -o /dev/null -w '%{http_code}' https://scurker.com`
-
-if [[ $HTTP_CODE != "200" ]]; then
-  echo "FAIL: status code $HTTP_CODE"
-  exit 1
-fi
